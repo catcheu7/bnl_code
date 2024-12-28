00eaa17f3c5aad52d73f5aad4922656abed0c47c
Catherine Cheu <89951748+catcheu7@users.noreply.github.com>
2024-12-28 12:52:14 -0500

    redo bounds

--- app_test3.py
+++ app_test3.py
@@ -9,6 +9,10 @@ class GDS():
         test = gdspy.GdsLibrary(infile = setter)
         cell = test.top_level()[0]
         polys = cell.get_polygons(by_spec = True)
+        bound = cell.get_bounding_box()
+        diff = bound[:,None] - bound[None,:]
+#diff
+#bound
 #polys
 
 class MainWindow(QMainWindow):
