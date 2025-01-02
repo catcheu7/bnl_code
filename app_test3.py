0d24d9df46f42cfb810a1a78e0820dc4665097f8
Catherine Cheu <89951748+catcheu7@users.noreply.github.com>
2025-01-02 11:57:03 -0500

    change verification

--- app_test3.py
+++ app_test3.py
@@ -12,7 +12,7 @@ class GDS():
         polys = cell.get_polygons(by_spec = True)
         bound = cell.get_bounding_box()
         diff = bound[:,None] - bound[None,:]
-        print(bound)
+        print(diff)
         return bound, diff
 
     def graphingbound(diff,bound):
