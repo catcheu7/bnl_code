cbb9ea1c5ca531af2d1e1c98564169ddb642ecc4
Catherine Cheu <89951748+catcheu7@users.noreply.github.com>
2025-01-03 11:26:33 -0500

    transfer from another jupyter to script

--- app_test3.py
+++ app_test3.py
@@ -24,13 +24,17 @@ class GDS():
         plt.ylim(bound[0][1],bound[1][1])
         return fig
 
-    def layered():
+    def layered(bound):
         count = 0
         for a,coords in polys.items():
             colors = ['black','red','blue','magenta','green','orange']
             for b in coords:
                 m = Polygon(b)
                 sepcoord = np.array(m.exterior.xy)
+                adjust = t - np.tile(np.array([[bound[0][0],bound[0][1]]]).transpose(),(1,t.shape[1]))
+                scaled = adjust//np.array([[xsize, ysize]]).transpose()
+                #print(scaled)
+                cor = Polygon(list(zip(scaled[0],scaled[1])))
                 plt.fill(*m.exterior.xy,color = colors[count])
             count +=1
         plt.savefig('test.png',dpi = 100)
