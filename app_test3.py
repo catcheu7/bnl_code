8a1679fd5556716bdd9994733b1d6f77b4381ca7
Catherine Cheu <89951748+catcheu7@users.noreply.github.com>
2025-01-06 15:16:52 -0500

    3d voxels

--- app_test3.py
+++ app_test3.py
@@ -111,7 +111,8 @@ class voxelview(QWidget):
 class graphsample(FigureCanvasQTAgg):
     def __init__(self):
         
-        ax = plt.figure().add_subplot()
+        ax = plt.figure()
+        self.ax1 = ax.add_subplot()
         super().__init__(ax)
         
 class graphwin(QWidget):
@@ -122,7 +123,8 @@ class graphwin(QWidget):
         self.setWindowTitle('Graph')
         graph = MainWindow.button_clicked()
         voxels = GDS.graph(graph)
-        canvas.draw(voxels)
+        canvas.self.ax1.plot(voxels)
+        canvas.draw()
         self.show()
 
 class MainWindow(QMainWindow):
