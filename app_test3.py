fb2760c66bb0b734888846ed1b646bdaeb2f37c8
Catherine Cheu <89951748+catcheu7@users.noreply.github.com>
2025-01-07 00:22:30 -0500

    redo

--- app_test3.py
+++ app_test3.py
@@ -48,6 +48,7 @@ class GDS():
             layername = 'layer' + str(count) + '.png'
             filenames.append(layername)
             fig.savefig(layername,dpi = 100)
+
             plt.show()
         return filenames
 
