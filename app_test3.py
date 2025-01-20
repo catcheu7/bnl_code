8abac837745a4b9442b0db04138eec917d9a49e1
Catherine Cheu <89951748+catcheu7@users.noreply.github.com>
2025-01-20 17:40:33 -0500

    transparency

--- app_test3.py
+++ app_test3.py
@@ -148,6 +148,7 @@ class graphwin(QWidget):
         colmat[:,:,:,0] = 0
         colmat[:,:,:,1] = 0
         colmat[:,:,:,2] = 255*self.sample/np.max(self.sample)
+        colmat[:,:,:,3] = 50
         self.canvas = graphsample()
         a,b,c = np.nonzero(sample)
         xdim = np.arange(0,x)
