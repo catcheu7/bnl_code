fd32fd0ef2631e2d1d1512eb309e3cdf811b8972
Catherine Cheu <89951748+catcheu7@users.noreply.github.com>
2025-01-22 16:08:39 -0500

    correct backend

--- app_test3.py
+++ app_test3.py
@@ -21,6 +21,7 @@ from pyqtgraph import functions as funcs
 from pyqtgraph.opengl import GLScatterPlotItem as glscatter
 import vispy
 from vispy import scene, io, app
+app.use_app('PySide6')
 #import /simu/simu_011325_v3.py as simu
 
 class GDS():
