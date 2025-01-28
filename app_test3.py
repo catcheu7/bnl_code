9b0e980b83a6c76508607aeae6de89f50d6bb4ca
Catherine Cheu <89951748+catcheu7@users.noreply.github.com>
2025-01-28 15:20:37 -0500

    redo

--- app_test3.py
+++ app_test3.py
@@ -196,7 +196,7 @@ class graphwin(QWidget):
         self.setWindowTitle('Graph Sample')
         """ for vert in vertshull.simplices:
          """    #self.canvas.ax1.plot3D(ones[vert,0],ones[vert,1],ones[vert,2])
-        plt.plot(x = anew,y = bnew,z = cnew,markersize = 1)
+        plt.scatter(anew,bnew,cnew,edgecolors='k', s = np.fill(1,len(anew)))
         #self.canvas.view_init(elev = 90, azim = 0)
         layform.addWidget(self.canvas)
         self.setLayout(layform)
