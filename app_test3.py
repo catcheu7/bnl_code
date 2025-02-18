b5b44dd1e88d62f84cc0032e28c4d67cbe5c5d6b
Catherine Cheu <89951748+catcheu7@users.noreply.github.com>
2025-02-18 17:27:54 -0500

    invert selection

--- app_test3.py
+++ app_test3.py
@@ -180,7 +180,7 @@ class graphwin(QWidget):
         gridview = viscan.central_widget.add_grid()
         viewer = gridview.add_view() """
         self.canvas = graphsample()
-        a,b,c = np.nonzero(self.sample)
+        a,b,c = np.where(self.sample == 0)
         #ones = np.asarray(list(zip(a,b,c)))
         
         #vertshull = ConvexHull(np.asarray(list(zip(a,b,c))))
