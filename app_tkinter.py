5f52b8dd5d7c2f1ba4723a16bad8a5198c4cd233
Catherine Cheu <89951748+catcheu7@users.noreply.github.com>
2025-04-22 14:35:20 -0400

    svg

--- app_tkinter.py
+++ app_tkinter.py
@@ -49,7 +49,7 @@ class GDS:
             a,b = layer_id.get_svg_classes()
             layers = set(layer for layer,typ in a.union(b))
             for sub in layers:
-                style = {(layer,typ) : {'fill-opacity : 0','style-opacity : 0'}
+                style = {(layer,typ) : {"fill-opacity" : "0","style-opacity" : "0"}
                 for layer,typ in a
                 if layer != sub
                 }
